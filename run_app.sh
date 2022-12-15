040faa2a344737e3b139892f8eb1d369acd09ab1
Johanna <johabbela@gmail.com>
2022-12-16 09:52:25 +1100

    Working on bash file and installed packages

--- run_app.sh
+++ run_app.sh
@@ -1,2 +1,14 @@
-pwd
-python menu.py
+echo "Hi"
+
+echo "Who are you"
+
+read Who
+
+echo "Welcome , $who!"
+
+
+#Create  a vnenv enviroment 
+# python3 -m vnenv./ vnenv
+
+#Once you activate the 
+# python menu.py
